77f318b983403977f269c223d8bc5aedbf22846f
e-hamada <ehamada@post.kek.jp>
2022-01-05 19:45:47 +0900

    Delete tmp2/tenmon_fake_files directory

--- tmp2/tenmon_fake_files/moonage.js
+++
@@ -1,9 +0,0 @@
-function moonage(selector){
-	if (undefined != jQuery) {
-		$(document).ready(function(){
-			$.get('/widget/moonage/moonage-j.shtml', function(data){
-				$(selector).html(data);
-			})
-		});
-	}
-}
